ba0856568b0f9ac9dba4baf793bb0061ada8a90f
António Martins <antonio.martins@officelaw.com>
2015-01-22 20:32:30 +0000

    Added categories check boxes to post form.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,7 +44,7 @@ class PostsController < ApplicationController
   private
 
     def post_params
-      params.require(:post).permit(:title, :url, :description)
+      params.require(:post).permit(:title, :url, :description, category_ids: [])
     end
 
 end
