3ff654bcfde599d952ccae1ffea19f16d2f8f2ee
António Martins <antonio.martins@officelaw.com>
2015-01-23 15:48:41 +0000

    Corrected migration posts table change to reference a user

--- db/migrate/20150112120953_add_posts_to_user.rb
+++ db/migrate/20150112120953_add_posts_to_user.rb
@@ -1,6 +1,7 @@
 class AddPostsToUser < ActiveRecord::Migration
   def change
-    add_reference :posts, :user, index:true
-  end
+    change_table :posts do |t|
+      t.references :user, index:true
+    end
   add_foreign_key :posts, :users
 end
