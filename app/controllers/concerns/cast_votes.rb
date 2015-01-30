829f701fd8fec34428e753a4acce0e4a6299aa57
António Martins <antonio.martins@officelaw.com>
2015-01-30 11:38:57 +0000

    Fixed a bug, when voting in a user profile, the tab would be the right one when a vote was cast.

--- app/controllers/concerns/cast_votes.rb
+++ app/controllers/concerns/cast_votes.rb
@@ -4,7 +4,8 @@ module CastVotes
   private
 
   def cast_vote(voteable)
-    Vote.create(value: params[:value], creator: current_user, voteable: voteable)
+    session[:voteable_type] = voteable.class.name
+    return Vote.create(value: params[:value], creator: current_user, voteable: voteable)
   end
 
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+
+  def active_tab
+    if session[:voteable_type].nil?
+      return 'Post'
+    elsif session[:voteable_type] == 'Post'
+      return 'Post'
+    else
+      return 'Comment'
+    end
+  end
+
 end
