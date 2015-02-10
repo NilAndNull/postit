cb35f56d001ec4088defbd6eacedfdd46b530ec5
António Martins <antonio.martins@officelaw.com>
2015-02-10 14:56:32 +0000

    Before Ajax assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
   end
 
   private
-    
+
     def user_params
       params.require(:user).permit(:username, :password, :password_confirmation)
     end
